6aa53493a66242c15a63f2518544a5af8bc28025
Ash <akshatchheda608@gmail.com>
2026-09-22 16:00:50 +0530

    Delete firmware/config.h

--- firmware/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-const char* WIFI_SSID     = "Your_Actual_WiFi_Name";
-const char* WIFI_PASSWORD = "Your_Actual_WiFi_Password";
-const char* SERVER_URL    = "https://plantpal-iot.onrender.com/api/sensor-data";
-
-#endif
